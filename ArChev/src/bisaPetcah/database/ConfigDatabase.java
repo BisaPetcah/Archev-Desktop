abb2fda721fdcf8cd9e606211155ecc0ae56e0e7
yazid138 <ma39isy@gmail.com>
2022-01-04 15:03:05 +0700

    commit lagi

--- ArChev/src/bisaPetcah/database/ConfigDatabase.java
+++ ArChev/src/bisaPetcah/database/ConfigDatabase.java
@@ -11,20 +11,21 @@ import io.github.cdimascio.dotenv.Dotenv;
  * @author ma39i
  */
 public class ConfigDatabase {
-    private static final Dotenv env = Dotenv.load();
+
+   private static final Dotenv env = Dotenv.load();
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String DB_URL = "jdbc:mysql://"+ env.get("DB_HOST") +"/" + env.get("DB_NAME");
-    private static final String DB_USER = env.get("DB_USER");
-    private static final String DB_PASSWORD = env.get("DB_PASSWORD");
-    
-   public static String getDriver() {
-       return ConfigDatabase.DRIVER;
-   }
-   
-   public static String getDbUrl() {
-       return ConfigDatabase.DB_URL;
-   }
-   
+   private static final String DB_URL = "jdbc:mysql://"+ env.get("DB_HOST") +"/" + env.get("DB_NAME");
+   private static final String DB_USER = env.get("DB_USER");
+   private static final String DB_PASSWORD = env.get("DB_PASSWORD");
+
+    public static String getDriver() {
+        return ConfigDatabase.DRIVER;
+    }
+
+    public static String getDbUrl() {
+        return ConfigDatabase.DB_URL;
+    }
+
     public static String getDbUser() {
        return ConfigDatabase.DB_USER;
    }
